09fbb51a112b5894b2d5b9a693c6a30cb98bc8d2
ibneytousif <itbangladesh.47@gmail.com>
2026-04-13 15:37:18 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Countries ></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,7 @@
+export default function Countries() {
+    return (
+        <div>
+            <h1 className="box-style">Hello world</h1>
+        </div>
+    )
+}
